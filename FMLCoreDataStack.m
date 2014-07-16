9581929dae139a9222308102a812be9337f1e60f
Foamy Media <hello@foamymedia.com>
2014-07-16 09:22:41 +0100

    Create FMLCoreDataStack.m

---
+++ FMLCoreDataStack.m
@@ -0,0 +1,154 @@
+//
+//  FMLCoreDataStack.m
+//  StickerApp
+//
+//  Created by Foamy Media on 09/07/2014.
+//  Copyright (c) 2014 foamymedia. All rights reserved.
+//
+
+#import "FMLCoreDataStack.h"
+
+
+@implementation FMLCoreDataStack
+
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+
+
++(instancetype)defaultStack {
+    static FMLCoreDataStack *stack;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        stack = [[self alloc] init];
+    });
+    return stack;
+}
+
+#pragma mark - Core Data stack
+
+// Returns the managed object context for the application.
+// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+- (NSManagedObjectContext *)managedObjectContext
+{
+    if (_managedObjectContext != nil) {
+        return _managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return _managedObjectContext;
+}
+
+// Returns the managed object model for the application.
+// If the model doesn't already exist, it is created from the application's model.
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Sticker" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return _managedObjectModel;
+}
+
+// Returns the persistent store coordinator for the application.
+// If the coordinator doesn't already exist, it is created and the application's store added to it.
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+    if (_persistentStoreCoordinator != nil) {
+        return _persistentStoreCoordinator;
+    }
+    
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"StickerApp.sqlite"];
+    
+    NSError *error = nil;
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+        /*
+         Replace this implementation with code to handle the error appropriately.
+         
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+         
+         Typical reasons for an error here include:
+         * The persistent store is not accessible;
+         * The schema for the persistent store is incompatible with current managed object model.
+         Check the error message to determine what the actual problem was.
+         
+         
+         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
+         
+         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
+         * Simply deleting the existing store:
+         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
+         
+         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
+         @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
+         
+         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
+         
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return _persistentStoreCoordinator;
+}
+
+#pragma mark - Application's Documents directory
+
+// Returns the URL to the application's Documents directory.
+- (NSURL *)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+- (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
+
+-(void)ensureInitialLoad {
+    NSString *initialLoadKey = @"Initial Load";
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    
+    BOOL hasInitialLoad = [userDefaults boolForKey:initialLoadKey];
+    if (hasInitialLoad == NO) {
+        [userDefaults setBool:YES forKey:initialLoadKey];
+
+        {
+            NSArray *categoryNames = @[@"comic",@"speech", @"alphabet",@"vintagemale",@"random",@"love",@"handdrawn",@"baby",@"cute",@"animal", @"hipster"];
+            for (NSString *categoryName in categoryNames) {
+                StickerCategory *category = [NSEntityDescription insertNewObjectForEntityForName:@"StickerCategory" inManagedObjectContext:[FMLCoreDataStack defaultStack].managedObjectContext];
+                category.name = categoryName;
+                category.imageName = [[categoryName lowercaseString] stringByAppendingString: @"1"];
+                NSMutableArray *mutableArray = [[NSMutableArray alloc]init];
+                for (int i= 1; i < 31; i++) {
+                    Sticker *sticker = [NSEntityDescription insertNewObjectForEntityForName:@"Sticker" inManagedObjectContext:[FMLCoreDataStack defaultStack].managedObjectContext];
+                    sticker.imageName = [[categoryName lowercaseString] stringByAppendingString:[NSString stringWithFormat:@"%d",i]];
+                    [mutableArray addObject:sticker];
+                }
+                category.stickers = [NSOrderedSet orderedSetWithArray:mutableArray];
+            }
+        }
+
+        [[FMLCoreDataStack defaultStack] saveContext];
+        NSLog(@"Initial data loaded.");
+
+    }
+}
+
+
+@end
